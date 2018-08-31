6f11bb04cc62a9a4e6ec28c35466e9c77142b66d
akash-akya <akashh246@gmail.com>
2018-08-31 23:16:42 +0530

    Update the parser

--- test/preprocess/parser_test.exs
+++ test/preprocess/parser_test.exs
@@ -4,20 +4,23 @@ defmodule Exscheme.Preprocess.ParserTest do
 
   test "simple sexp" do
     expr = "(function arg 100)"
-    assert parse(expr) == ["function", "arg", "100"]
+    assert parse(expr) == [:function, :arg, 100]
 
-    expr = " (function arg 100)   "
-    assert parse(expr) == ["function", "arg", "100"]
-
-    expr = "(    function     arg \n100) "
-    assert parse(expr) == ["function", "arg", "100"]
-
-    expr = "(function arg 100)"
-    assert parse(expr) == ["function", "arg", "100"]
+    expr = "   (function    arg 100 ) "
+    assert parse(expr) == [:function, :arg, 100]
   end
 
   test "sexp with sub expression" do
     expr = "(function (add 200 300) 100)"
-    assert parse(expr) == ["function", ["add", "200", "300"], "100"]
+    assert parse(expr) == [:function, [:add, 200, 300], 100]
+
+    expr = "(function (add-test (+ 200) 300) 100 (if #t))"
+
+    assert parse(expr) == [
+             :function,
+             [String.to_atom("add-test"), [:+, 200], 300],
+             100,
+             [:if, String.to_atom("#t")]
+           ]
   end
 end
